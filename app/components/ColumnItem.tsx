de4f069c2ee287d96aa3255b0384c5a17dff81f7
Matt Aitken <matt@mattaitken.com>
2022-03-04 13:36:34 +0000

    ColumnItem: memoized calculations and added an inner component

--- app/components/ColumnItem.tsx
+++ app/components/ColumnItem.tsx
@@ -1,6 +1,6 @@
 import { ChevronRightIcon } from "@heroicons/react/outline";
 import { Mono } from "./Primitives/Mono";
-import { useEffect, useMemo, useRef } from "react";
+import { RefObject, useEffect, useMemo, useRef } from "react";
 import { ColumnViewNode } from "~/useColumnView";
 import {
   useJsonColumnViewAPI,
@@ -19,60 +19,93 @@ export function ColumnItem({ item }: { item: ColumnViewNode }) {
 
   const showArrow = children.length > 0;
 
-  const isHighlighted = (path: string[], id: string) => {
-    return path[path.length - 1] === id;
-  };
+  const isHighlighted = useMemo(() => {
+    if (highlightedPath.length === 0) return false;
+    return highlightedPath[highlightedPath.length - 1] === id;
+  }, [highlightedPath, id]);
 
-  const isSelected = (path: string[], id: string) => {
-    return path.includes(id);
-  };
+  const isSelected = useMemo(() => {
+    return selectedPath.includes(id);
+  }, [selectedPath, id]);
 
   const stateStyle = useMemo<string>(() => {
-    if (highlightedPath.length === 0) {
-      return "";
-    }
-
-    if (isHighlighted(highlightedPath, id)) {
+    if (isHighlighted) {
       return "bg-slate-300 text-slate-700 hover:bg-slate-400 hover:bg-opacity-60 transition duration-75 ease-out dark:bg-white dark:bg-opacity-[15%] dark:text-slate-400";
     }
 
-    if (isSelected(selectedPath, id)) {
+    if (isSelected) {
       return "bg-slate-200 hover:bg-slate-300 transition duration-75 ease-out dark:bg-white dark:bg-opacity-[5%] dark:hover:bg-white dark:hover:bg-opacity-[10%] dark:text-slate-400";
     }
 
     return "hover:bg-slate-100 transition duration-75 ease-out dark:hover:bg-white dark:hover:bg-opacity-[5%] dark:text-slate-400";
-  }, [highlightedPath, selectedPath, id]);
+  }, [isSelected, isHighlighted]);
 
-  const iconColor = colorForItemAtPath(id, json);
+  const iconColor = useMemo<string>(
+    () => colorForItemAtPath(id, json),
+    [id, json]
+  );
 
   useEffect(() => {
-    if (isSelected(selectedPath, id) || isHighlighted(highlightedPath, id)) {
+    if (isSelected || isHighlighted) {
       htmlElement.current?.scrollIntoView({ block: "nearest" });
     }
-  }, [highlightedPath, selectedPath, id]);
+  }, [isSelected, isHighlighted]);
 
+  const memoComponent = useMemo(() => {
+    return (
+      <ColumnItemInner
+        stateStyle={stateStyle}
+        onClick={() => goToNodeId(id)}
+        item={item}
+        iconColor={iconColor}
+        showArrow={showArrow}
+        divRef={htmlElement}
+      />
+    );
+  }, [goToNodeId, item, isSelected, isHighlighted]);
+
+  return memoComponent;
+}
+
+type ColumnItemInnerProps = {
+  stateStyle: string;
+  onClick: () => void;
+  item: ColumnViewNode;
+  iconColor: string;
+  showArrow: boolean;
+  divRef: RefObject<HTMLDivElement>;
+};
+
+function ColumnItemInner({
+  stateStyle,
+  onClick,
+  item,
+  iconColor,
+  showArrow,
+  divRef,
+}: ColumnItemInnerProps) {
   return (
     <div
       className={`flex h-9 items-center justify-items-stretch mx-1 px-1 py-1 my-1 rounded-sm ${stateStyle}`}
-      onClick={() => goToNodeId(id)}
-      ref={htmlElement}
+      onClick={() => onClick()}
+      ref={divRef}
     >
       <div className="w-4 flex-none flex-col justify-items-center">
-        {item.icon && <item.icon className={`${iconColor} h-5 w-5`} />}
+        {/* {item.icon && <item.icon className={`${iconColor} h-5 w-5`} />} */}
       </div>
 
       <div className="flex flex-grow flex-shrink items-baseline justify-between truncate">
-        <Body className="flex-grow flex-shrink-0 pl-3 pr-2">{title}</Body>
-        {subtitle && (
+        <Body className="flex-grow flex-shrink-0 pl-3 pr-2">{item.title}</Body>
+        {item.subtitle && (
           <Mono className="truncate text-gray-400 pr-1 transition dark:text-gray-500">
-            {subtitle}
+            {item.subtitle}
           </Mono>
         )}
       </div>
 
-      {showArrow && (
+      {/* {showArrow && (
         <ChevronRightIcon className="flex-none w-4 h-4 text-gray-400" />
-      )}
+      )} */}
     </div>
   );
 }
