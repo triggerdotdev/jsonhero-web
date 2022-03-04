fdf1d0282f8621c7adb5eb48fc7cd605f1e7836d
Eric Allam <eallam@icloud.com>
2022-03-04 21:28:14 +0000

    Cleanup JsonColumnView

--- app/components/JsonColumnView.tsx
+++ app/components/JsonColumnView.tsx
@@ -1,37 +1,12 @@
-import { memo, useMemo } from "react";
-import { Column } from "./Column";
-import { ColumnItem } from "./ColumnItem";
-import { ScrollingColumnView } from "./ScrollingColumnView";
 import {
   useJsonColumnViewAPI,
   useJsonColumnViewState,
 } from "../hooks/useJsonColumnView";
-import { useJson } from "~/hooks/useJson";
-import { JSONHeroPath } from "@jsonhero/path";
-import { inferType } from "@jsonhero/json-infer-types";
 import { useHotkeys } from "react-hotkeys-hook";
 import { Columns } from "./Columns";
 
 export function JsonColumnView() {
-  const { getColumnViewProps, columns, highlightedPath, selectedPath } =
-    useJsonColumnViewState();
-  const [json] = useJson();
-
-  const addBlankColumn = useMemo<boolean>(() => {
-    if (columns.length === 0) return true;
-
-    const deepestElementId = selectedPath[selectedPath.length - 1];
-    const heroPath = new JSONHeroPath(deepestElementId);
-    const value = heroPath.first(json);
-    const item = inferType(value);
-
-    if (item.name === "array" && item.value.length === 0) return true;
-    if (item.name === "object" && Object.keys(item.value).length === 0)
-      return true;
-
-    const isContainer = item.name === "array" || item.name === "object";
-    return !isContainer;
-  }, [columns, selectedPath]);
+  const { getColumnViewProps, columns } = useJsonColumnViewState();
 
   return (
     <>
