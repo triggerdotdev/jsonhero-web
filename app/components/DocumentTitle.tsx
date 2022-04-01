a4bbd5521e1cd3ad8a086bfe263622067736a650
James Ritchie <james@jamesritchie.co.uk>
2022-04-01 14:05:01 +0100

    Improved the title editing in the header

--- app/components/DocumentTitle.tsx
+++ app/components/DocumentTitle.tsx
@@ -27,26 +27,24 @@ export function DocumentTitle() {
         className="flex justify-center items-center w-full"
         title={doc.title}
       >
-        <input
-          ref={ref}
-          className={
-            "min-w-[15vw] border-none text-center text-ellipsis text-slate-300 px-2 pl-3 py-1 rounded-sm bg-transparent placeholder:text-slate-400 focus:bg-black focus:bg-opacity-30 focus:outline-none focus:border-none hover:bg-black hover:bg-opacity-30 hover:cursor-text transition dark:bg-transparent dark:text-slate-200 dark:placeholder:text-slate-400 dark:focus:bg-black dark:focus:bg-opacity-10 dark:hover:bg-black dark:hover:bg-opacity-10"
-          }
-          type="text"
-          name="title"
-          spellCheck="false"
-          placeholder="Name your JSON file"
-          value={editedTitle}
-          onChange={(e) => setEditedTitle(e.target.value)}
-        />
+        <label className="relative block group">
+          <PencilAltIcon className="h-5 w-5 absolute top-1/2 transform -translate-y-1/2 left-3 text-white opacity-0 transition pointer-events-none group-hover:opacity-80 group-focus:opacity-80" />
+          <input
+            ref={ref}
+            className={
+              "min-w-[15vw] border-none text-ellipsis text-slate-300 px-2 pl-10 py-1 rounded-sm bg-transparent placeholder:text-slate-400 focus:bg-black/30 focus:outline-none focus:border-none hover:bg-black hover:bg-opacity-30 hover:cursor-text transition dark:bg-transparent dark:text-slate-200 dark:placeholder:text-slate-400 dark:focus:bg-black dark:focus:bg-opacity-10 dark:hover:bg-black dark:hover:bg-opacity-10"
+            }
+            type="text"
+            name="title"
+            spellCheck="false"
+            placeholder="Name your JSON file"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+        </label>
 
         {match(editedTitle)
-          .with(doc.title, () => (
-            <PencilAltIcon
-              className="h-5 w-5 text-black text-opacity-50"
-              onClick={startEditing}
-            />
-          ))
+          .with(doc.title, () => <p className="ml-2 text-transparent">Save</p>)
           .with("", () => (
             <button
               className="ml-2 text-lime-500 hover:text-lime-600 transition"
