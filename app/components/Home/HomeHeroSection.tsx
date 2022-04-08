1fbedc5e063f24505d0734b13715c250f3edb608
Matt Aitken <matt@mattaitken.com>
2022-04-08 17:42:55 +0100

    Added the new homepage video, and improved the layout more

--- app/components/Home/HomeHeroSection.tsx
+++ app/components/Home/HomeHeroSection.tsx
@@ -10,27 +10,27 @@ const jsonHeroSlogan = "But we're making it better.";
 export function HomeHeroSection() {
   return (
     <div
-      className={`flex items-stretch bg-indigo-900 p-6 pb-16 pt-32 md:pt-32`}
+      className={`flex items-stretch flex-col md:flex-row bg-[rgb(55,52,139)] p-6 pb-16 pt-32 md:pt-32`}
     >
-      <div className={`flex flex-col md:flex-row`}>
-        <div className="self-center mt-6 md:pr-10">
+      <div className="self-center mt-6 md:w-1/2 md:pr-10 flex justify-end">
+        <div className=" max-w-3xl">
           <AutoplayVideo src="/home/JsonHero2.mp4" />
         </div>
-        <div className="self-center flex align-center md:w-1/2 mt-8 lg:mt-0">
-          <div className="max-w-lg">
-            <ExtraLargeTitle className="text-lime-300">
-              {jsonHeroTitle}
-            </ExtraLargeTitle>
-            <ExtraLargeTitle className="text-white mb-4">
-              {jsonHeroSlogan}
-            </ExtraLargeTitle>
-            <SmallSubtitle className="text-slate-400 mb-8">
-              Stop staring at thousand line JSON files in your editor and start
-              staring at thousand line JSON files in your browser. With a few
-              nice features to help make it not <em>the worst</em>.
-            </SmallSubtitle>
-            <NewFile />
-          </div>
+      </div>
+      <div className="self-center flex align-center md:w-1/2 mt-8 lg:mt-0">
+        <div className="max-w-lg">
+          <ExtraLargeTitle className="text-lime-300">
+            {jsonHeroTitle}
+          </ExtraLargeTitle>
+          <ExtraLargeTitle className="text-white mb-4">
+            {jsonHeroSlogan}
+          </ExtraLargeTitle>
+          <SmallSubtitle className="text-slate-400 mb-8">
+            Stop staring at thousand line JSON files in your editor and start
+            staring at thousand line JSON files in your browser. With a few nice
+            features to help make it not <em>the worst</em>.
+          </SmallSubtitle>
+          <NewFile />
         </div>
       </div>
     </div>
