7bf65632ecbf166489a4a93d4f1c307d5d1c9149
Matt Aitken <matt@mattaitken.com>
2022-04-08 16:37:51 +0100

    Much larger video for the homepage hero section

--- app/components/Home/HomeHeroSection.tsx
+++ app/components/Home/HomeHeroSection.tsx
@@ -9,24 +9,30 @@ const jsonHeroSlogan = "But we're making it better.";
 
 export function HomeHeroSection() {
   return (
-    <HomeSection containerClassName="md:h-[80vh] bg-black p-6 pb-16 pt-32 md:pt-48">
-      <div className="mt-6 lg:w-1/2 md:pr-10">
-        <AutoplayVideo src="/home/JsonHero2.mp4" />
+    <div
+      className={`flex items-stretch bg-indigo-900 p-6 pb-16 pt-32 md:pt-32`}
+    >
+      <div className={`flex flex-col md:flex-row`}>
+        <div className="self-center mt-6 md:pr-10">
+          <AutoplayVideo src="/home/JsonHero2.mp4" />
+        </div>
+        <div className="self-center flex align-center md:w-1/2 mt-8 lg:mt-0">
+          <div className="max-w-lg">
+            <ExtraLargeTitle className="text-lime-300">
+              {jsonHeroTitle}
+            </ExtraLargeTitle>
+            <ExtraLargeTitle className="text-white mb-4">
+              {jsonHeroSlogan}
+            </ExtraLargeTitle>
+            <SmallSubtitle className="text-slate-400 mb-8">
+              Stop staring at thousand line JSON files in your editor and start
+              staring at thousand line JSON files in your browser. With a few
+              nice features to help make it not <em>the worst</em>.
+            </SmallSubtitle>
+            <NewFile />
+          </div>
+        </div>
       </div>
-      <div className="lg:w-1/2">
-        <ExtraLargeTitle className="text-lime-300">
-          {jsonHeroTitle}
-        </ExtraLargeTitle>
-        <ExtraLargeTitle className="text-white mb-4">
-          {jsonHeroSlogan}
-        </ExtraLargeTitle>
-        <SmallSubtitle className="text-slate-400 mb-8">
-          Stop staring at thousand line JSON files in your editor and start
-          staring at thousand line JSON files in your browser. With a few nice
-          features to help make it not <em>the worst</em>.
-        </SmallSubtitle>
-        <NewFile />
-      </div>
-    </HomeSection>
+    </div>
   );
 }
