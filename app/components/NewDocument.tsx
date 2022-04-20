be43460feae23fefe5a39051f85eb1fae83fbbdd
Eric Allam <eallam@icloud.com>
2022-04-20 10:56:42 +0100

    Make the NewDocument popover a little wider

--- app/components/NewDocument.tsx
+++ app/components/NewDocument.tsx
@@ -5,7 +5,7 @@ import { UrlForm } from "./UrlForm";
 
 export function NewDocument() {
   return (
-    <div className="bg-indigo-700 text-white rounded-sm shadow-md w-80 max-w-max p-3 transition">
+    <div className="bg-indigo-700 text-white rounded-sm shadow-md w-96 max-w-max p-3 transition">
       <div className="flex flex-col">
         <UrlForm className="mb-2" />
         <DragAndDropForm />
