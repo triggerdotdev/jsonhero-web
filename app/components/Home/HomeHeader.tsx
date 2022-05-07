35082d9c438040921a7e0db94e7f7cf776bc226b
James Ritchie <james@jamesritchie.co.uk>
2022-05-07 18:33:32 +0100

    Small header tweak to fix mobile portrait

--- app/components/Home/HomeHeader.tsx
+++ app/components/Home/HomeHeader.tsx
@@ -14,7 +14,7 @@ import {
 export function HomeHeader() {
   return (
     <header className="fixed z-20 flex items-center justify-between w-screen h-[82px] px-4 bg-indigo-700">
-      <div className="flex w-44 mr-3">
+      <div className="flex w-36 sm:w-44 mr-3">
         <Logo />
       </div>
       <ol className="flex items-center gap-2">
