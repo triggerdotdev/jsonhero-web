d7225b201f36482044dcbf55cd16bb69bea850a4
James Ritchie <james@jamesritchie.co.uk>
2022-06-07 13:55:37 +0100

    Added Product Hunt Banner to homepage

---
+++ app/components/Home/HomeProductHuntBanner.tsx
@@ -0,0 +1,23 @@
+import { Body } from "../Primitives/Body";
+
+export function HomeProductHuntBanner() {
+  return (
+    <div className="flex items-center justify-center w-full py-3 bg-indigo-600">
+      <div className="flex items-center">
+        <Body className="mr-3 text-xl text-white">
+          We're live on Product Hunt! 👉
+        </Body>
+        <a
+          href="https://www.producthunt.com/posts/json-hero-chrome-extension?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-json&#0045;hero&#0045;chrome&#0045;extension"
+          target="_blank"
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=348656&theme=light"
+            alt="JSON&#0032;Hero&#0032;Chrome&#0032;extension - A&#0032;beautiful&#0032;JSON&#0032;explorer&#0032;packed&#0032;with&#0032;features | Product Hunt"
+            className="h-14"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,6 +7,7 @@ import { HomeInfoBoxSection } from "~/components/Home/HomeInfoBoxSection";
 import { HomeSearchSection } from "~/components/Home/HomeSearchSection";
 import { HomeFooter } from "~/components/Home/HomeFooter";
 import { GithubBanner } from "~/components/Home/HomeGithubBanner";
+import { HomeProductHuntBanner } from "~/components/Home/HomeProductHuntBanner";
 import { HomeChromeExtensionPrompt } from "~/components/Home/HomeChromeExtensionPrompt";
 import { LoaderFunction, useLoaderData } from "remix";
 import userAgentParser from "ua-parser-js";
@@ -33,12 +34,12 @@ export default function Index() {
     <div className="overflow-x-hidden">
       <HomeHeader fixed={true} />
       <HomeHeroSection />
-      {userAgent.browser.name === "Chrome" ? (
+      {/* {userAgent.browser.name === "Chrome" ? (
         <HomeChromeExtensionPrompt />
       ) : (
         <GithubBanner />
-      )}
-
+      )} */}
+      <HomeProductHuntBanner />
       <HomeInfoBoxSection />
       <HomeEdgeCasesSection />
       <HomeSearchSection />
