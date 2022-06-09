25e1b20d9c17181339077199c6d6329b634db61f
Eric Allam <eallam@icloud.com>
2022-06-09 15:12:05 +0100

    Remove Chrome extension banner

--- app/components/Icons/ChromeExtensionBadge.tsx
+++
@@ -1,5 +0,0 @@
-import chromeExtensionBadge from "~/assets/images/chrome-extension-badge.png";
-
-export function ChromeExtensionBadge() {
-  return <img src={chromeExtensionBadge} className="h-14"></img>;
-}
