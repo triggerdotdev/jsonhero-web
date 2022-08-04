2d6b36f7b723c00881dbd04621773b37bf02e3d6
D-K-P <8297864+D-K-P@users.noreply.github.com>
2022-08-04 13:26:56 +0100

    Updated the API Hero banner

--- app/components/Home/HomeApiHeroBanner.tsx
+++ app/components/Home/HomeApiHeroBanner.tsx
@@ -3,11 +3,11 @@ import { HomeApiHeroLaptop } from "./HomeApiHeroLaptop";
 
 export function HomeApiHeroBanner() {
   return (
-    <div className="flex items-center justify-start md:justify-center w-full h-40 bg-gradient-to-r from-blue-600 via-purple-600 to-indigo-600 hover:backdrop-filter hover:backdrop-brightness-75 transition">
+    <div className="flex items-center justify-start md:justify-center w-full h-40 bg-gradient-to-r from-blue-600 via-pink-500 to-indigo-600 hover:backdrop-filter hover:backdrop-brightness-75 transition">
       <div className="relative flex justify-center items-center w-1/2 md:w-full pl-6 md:px-6">
         <div className="flex flex-col">
-          <Body className="text-white text-2xl md:text-4xl leading-tight">
-            Introducing…
+          <Body className="text-white text-2xl font-bold md:text-4xl leading-tight">
+            We did a thing!
           </Body>
           <p className="mb-2 text-white">
             <span className="font-bold">API Hero</span> – integrate APIs in
@@ -16,9 +16,9 @@ export function HomeApiHeroBanner() {
           <a
             href="https://apihero.run"
             target="new"
-            className="flex items-center justify-center px-3 py-2 mt-2 text-center text-lg md:text-xl text-white bg-rose-500 rounded shadow-md hover:bg-rose-600 transition"
+            className="flex items-center justify-center px-3 py-2 mt-2 text-center text-lg md:text-xl text-slate-800 font-bold bg-lime-500 rounded shadow-md hover:bg-lime-400 transition"
           >
-            Check it out 👉
+            Sign up for early access &rarr;
           </a>
         </div>
         <a
