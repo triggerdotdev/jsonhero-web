03be0298e0f651e405e6c2a7604a17efa35097db
kvooak <me@kvooak.io>
2022-09-12 18:17:08 +0200

    Check if string input isXML()

---
+++ app/utilities/xml/isXML.ts
@@ -0,0 +1,15 @@
+export default function isXML(possibleXml: string): boolean {
+  console.log(window.DOMParser);
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(possibleXml, "application/xml");
+  // As per this latest documentation
+  // https://developer.mozilla.org/en-US/docs/Web/API/DOMParser/parseFromString
+  // DOMParser.parseFromString() will not throw an error if the XML is invalid
+  // there for this code below and not try/catch
+  const errorNode = xmlDoc.querySelector("parsererror");
+  if (errorNode) {
+    console.error(`XML parsing error, ${errorNode.textContent}`);
+    return false;
+  }
+  return true;
+}
