8e8dae22d1c9b7cb7263dd38a099ff5a75142179
Quang Hoang <kvooak@icloud.com>
2022-09-19 13:10:57 +0200

    cleanup

--- app/theme.server.ts
+++ app/theme.server.ts
@@ -2,7 +2,7 @@ import { createCookieSessionStorage } from "remix";
 
 import { Theme, isTheme } from "~/components/ThemeProvider";
 
-const sessionSecret = 'SESSION_SECRET';
+const sessionSecret = SESSION_SECRET;
 
 // if (!sessionSecret) {
 //   throw new Error("SESSION_SECRET must be set");
