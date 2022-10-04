c8a16f115a462564d472e5b747aebd82bf47841f
ShaneMaglangit <shanedmaglangit@gmail.com>
2022-10-04 11:25:33 +0800

    Feat: Add loader on index.tsx to fetch data returned when redirected from form submission routes.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,19 +7,34 @@ import { HomeInfoBoxSection } from "~/components/Home/HomeInfoBoxSection";
 import { HomeSearchSection } from "~/components/Home/HomeSearchSection";
 import { HomeFooter } from "~/components/Home/HomeFooter";
 import { HomeApiHeroBanner } from "~/components/Home/HomeApiHeroBanner";
+import { json, LoaderFunction, useLoaderData } from "remix";
+import { commitSession, getSession } from "~/session.server";
+import { uuid } from "~/utilities/uuid";
+
+export interface JsonFormResponse {
+  key: string;
+  malformedError?: boolean;
+}
+
+export const loader: LoaderFunction = async ({request}) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  const response: JsonFormResponse = {key: uuid(), malformedError: session.get("malformedError") ?? false}
+  return json(response, {headers: {"Set-Cookie": await commitSession(session)}});
+};
 
 export default function Index() {
+  const formResponse = useLoaderData<JsonFormResponse>();
   return (
     <div className="overflow-x-hidden">
-      <HomeHeader fixed={true} />
-      <HomeHeroSection />
-      <HomeApiHeroBanner />
-      <HomeInfoBoxSection />
-      <HomeEdgeCasesSection />
-      <HomeSearchSection />
-      <HomeCollaborateSection />
-      <HomeFeatureGridSection />
-      <HomeFooter />
+      <HomeHeader fixed={true}/>
+      <HomeHeroSection formResponse={formResponse}/>
+      <HomeApiHeroBanner/>
+      <HomeInfoBoxSection/>
+      <HomeEdgeCasesSection/>
+      <HomeSearchSection/>
+      <HomeCollaborateSection/>
+      <HomeFeatureGridSection/>
+      <HomeFooter/>
     </div>
   );
 }
