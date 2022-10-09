19e653facfa83ce296b68ff8897e44b2e098b077
Suven-p <pandeysuven@gmail.com>
2022-10-09 21:40:21 +0545

    Replace terminal with download pointing to raw view

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { TemplateIcon, CodeIcon, TerminalIcon } from "@heroicons/react/outline";
+import { TemplateIcon, CodeIcon, DownloadIcon } from "@heroicons/react/outline";
 import { TreeIcon } from "~/components/Icons/TreeIcon";
 import { useHotkeys } from "react-hotkeys-hook";
 import { Link, useLocation, useNavigate } from "remix";
@@ -52,17 +52,13 @@ export function SideBar() {
         </SidebarLink>
       </ol>
       <ol>
-        <SidebarLink to={`/j/${doc.id}/terminal`} hotKey="option+4,alt+4">
-          <ToolTip arrow="left">
-            <Body>Terminal</Body>
-            <ShortcutIcon className="w-[26px] h-[26px] ml-1 text-slate-700 bg-slate-200 dark:text-slate-300 dark:bg-slate-800">
-              ⌥
-            </ShortcutIcon>
-            <ShortcutIcon className="w-[26px] h-[26px] ml-1 text-slate-700 bg-slate-200 dark:text-slate-300 dark:bg-slate-800">
-              4
-            </ShortcutIcon>
-          </ToolTip>
-          <TerminalIcon className="p-2 w-full h-full" />
+        <SidebarLink>
+          <a href={`/j/${doc.id}.json`} target="_blank">
+            <ToolTip arrow="left">
+              <Body>Download</Body>
+            </ToolTip>
+            <DownloadIcon className="p-2 w-full h-full" />
+          </a>
         </SidebarLink>
       </ol>
     </div>
@@ -95,9 +91,8 @@ function SidebarLink({
     }
   }
 
-  const href = `${to}${
-    queryParams.toString().length > 0 ? `?${queryParams.toString()}` : ""
-  }`;
+  const href = `${to}${queryParams.toString().length > 0 ? `?${queryParams.toString()}` : ""
+    }`;
 
   if (hotKey) {
     const navigate = useNavigate();
