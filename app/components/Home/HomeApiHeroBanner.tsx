531b15f3340542e4e9d20d18aa9389f1f42928fb
James Ritchie <james@jamesritchie.co.uk>
2022-10-09 09:52:44 +0100

    Changed banner wording to promote API Hero open access

--- app/components/Home/HomeApiHeroBanner.tsx
+++ app/components/Home/HomeApiHeroBanner.tsx
@@ -6,11 +6,8 @@ export function HomeApiHeroBanner() {
     <div className="flex items-center justify-start md:justify-center w-full h-40 bg-gradient-to-r from-purple-600 via-pink-500 to-purple-600 hover:backdrop-filter hover:backdrop-brightness-75 transition">
       <div className="relative flex justify-center items-center w-1/2 md:w-full pl-6 md:px-6">
         <div className="flex flex-col">
-          <Body className=" text-white text-2xl  md:text-3xl leading-tight">
-            <span className="hidden md:inline">Introducing... </span>
-            <span className="font-bold">
-              <a href="https://apihero.run/">⚡️ API Hero.</a>
-            </span>
+          <Body className=" text-white text-[1rem] sm:text-[1.2rem] font-bold md:text-3xl leading-tight">
+            Introducing ⚡️ API Hero
           </Body>
           <p className="mb-2 text-white">
             Integrate APIs in seconds
@@ -23,10 +20,7 @@ export function HomeApiHeroBanner() {
             target="new"
             className="flex items-center justify-center px-3 py-2 mt-2 text-center text-m md:text-xl text-slate-800 font-bold bg-lime-500 rounded shadow-md hover:bg-lime-400 transition"
           >
-            Free private beta{" "}
-            <span className="hidden md:inline">&nbsp;starting soon </span>
-            <span className="md:hidden">&nbsp; </span>
-            &rarr;
+            Check it out &rarr;
           </a>
         </div>
         <a
