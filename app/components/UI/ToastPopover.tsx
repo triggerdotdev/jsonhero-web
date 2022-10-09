423a55c202e28ef1380b4c15f65a8fd1b1d665f6
James Ritchie <james@jamesritchie.co.uk>
2022-10-09 11:41:16 +0100

    Small style update to the Toast

--- app/components/UI/ToastPopover.tsx
+++ app/components/UI/ToastPopover.tsx
@@ -1,3 +1,7 @@
+import {
+  ExclamationCircleIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/outline";
 import * as ToastPrimitive from "@radix-ui/react-toast";
 import cx from "~/utilities/classnames";
 import { Body } from "../Primitives/Body";
@@ -15,9 +19,9 @@ const Toast = ({
   duration?: number;
 }) => {
   const commonRootClasses = cx(
-    "z-50 fixed bottom-4 inset-x-4 w-auto md:bottom-4 md:right-2 md:left-auto md:bottom-auto md:w-full md:max-w-sm shadow-lg rounded-md",
+    "z-50 fixed top-4 left-4 py-2 w-auto md:top-4 md:right-4 md:left-auto md:top-auto md:w-full md:max-w-sm shadow-lg rounded-md",
     "border-[1px]",
-    "radix-state-open:animate-toast-slide-in-bottom md:radix-state-open:animate-toast-slide-in-right",
+    "radix-state-open:animate-toast-slide-in-top md:radix-state-open:animate-toast-slide-in-right",
     "radix-state-closed:animate-toast-hide",
     "radix-swipe-end:animate-toast-swipe-out",
     "translate-x-radix-toast-swipe-move-x",
@@ -27,20 +31,30 @@ const Toast = ({
 
   const typeRootClasses =
     type === "success"
-      ? "bg-slate-100 dark:bg-slate-900 border-slate-200 dark:border-black"
-      : "bg-red-100 dark:bg-red-100 border-red-200 dark:border-red-400";
+      ? "bg-slate-50 dark:bg-slate-900"
+      : "bg-rose-50 dark:bg-rose-100";
 
-  const titleClasses = type === "success" ? "text-slate-900" : "text-red-900";
-  const bodyClasses = type === "success" ? "text-slate-700" : "text-red-700";
+  const titleClasses = type === "success" ? "text-slate-900" : "text-slate-900";
+  const bodyClasses = type === "success" ? "text-slate-700" : "text-slate-700";
+
+  const iconType =
+    type === "success" ? "text-emerald-700 h-7 w-7" : "text-rose-700 h-7 w-7";
 
   return (
     <ToastPrimitive.Provider duration={duration ?? 2500}>
       <ToastPrimitive.Root className={cx(commonRootClasses, typeRootClasses)}>
         <div className="flex">
-          <div className="w-0 flex-1 flex items-center pl-5 py-4">
+          <div className="flex-1 flex items-center">
+            <div className="flex px-4">
+              {type === "success" ? (
+                <InformationCircleIcon className={cx(iconType)} />
+              ) : (
+                <ExclamationCircleIcon className={cx(iconType)} />
+              )}
+            </div>
             <div className="w-full radix">
-              <Title className={titleClasses}>{title}</Title>
-              <Body className={cx("mt-0.5", bodyClasses)}>{message}</Body>
+              <Title className={cx("-mb-0.5", titleClasses)}>{title}</Title>
+              <Body className={cx("mb-0.5", bodyClasses)}>{message}</Body>
             </div>
           </div>
         </div>
