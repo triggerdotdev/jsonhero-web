4d85098aeb205412275271a55525af8c6d9fecd7
Matt Aitken <matt@mattaitken.com>
2022-11-29 11:27:48 +0000

    Removed Unsplash and Airtable example docs

--- app/components/SampleUrls.tsx
+++ app/components/SampleUrls.tsx
@@ -5,23 +5,10 @@ export function SampleUrls() {
   return (
     <div className="flex justify-start flex-wrap gap-2">
       <ExampleDoc
-        id="xW6aDgcbKgwC"
-        title="Unsplash API"
-        path="4.urls.regular"
-      />
-
-      <ExampleDoc
         id="d9udW60cLOok"
         title="Tweet JSON"
         path="data.0.entities.urls.0.expanded_url"
       />
-
-      <ExampleDoc
-        id="JZw4Wx9Mgj6r"
-        title="Airtable API"
-        path="records.3.createdTime"
-      />
-
       <ExampleDoc id="PjHo1o5MVeH4" title="Github API" />
     </div>
   );
