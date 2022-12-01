0bccc27347c8577829ec759f1eb7db05ad178d43
D-K-P <dkp.github@pm.me>
2022-12-01 17:51:06 +0000

    Added the workflow link to the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -44,7 +44,7 @@ export function Footer() {
           </Body>
         </li>
       </ol>
-
+      <WorkflowFooterBanner />
       <ol className="flex gap-2 items-center h-full invisible md:visible">
         {minimal && (
           <li>
@@ -59,5 +59,23 @@ export function Footer() {
         </li>
       </ol>
     </footer>
+  );
+}
+
+function WorkflowFooterBanner() {
+  return (
+    <a
+      href="https://bcymafitv0e.typeform.com/workflows"
+      target="_blank"
+      className="hover:underline flex group hover:cursor-pointer text-slate-900 dark:text-white transition items-center dark:hover:text-transparent dark:hover:bg-clip-text dark:hover:bg-gradient-to-r dark:hover:from-purple-400 dark:hover:to-pink-600"
+    >
+      <p className="md:text-lg text-sm whitespace-nowrap">
+        🚀 Help us out -
+        <span className=" font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">
+          &nbsp; Take our 2 min survey
+        </span>
+      </p>
+      <ArrowRightIcon className="ml-1 h-4 w-4 text-base text-slate-900 dark:text-white dark:group-hover:text-purple-400 transition" />
+    </a>
   );
 }
