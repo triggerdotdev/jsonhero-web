59f223babac4c2f959b89a806ce892a2f8d7dd0e
James Ritchie <james@jamesritchie.co.uk>
2023-01-04 15:48:54 +0000

    Small change

--- app/components/Home/HomeFooter.tsx
+++ app/components/Home/HomeFooter.tsx
@@ -13,7 +13,7 @@ export function HomeFooter({ maxWidth = "1150px" }: HomeFooterProps) {
   return (
     <footer className="flex flex-col items-center w-full px-4 py-6 bg-black md:py-10">
       <div
-        className="flex items-center justify-between w-full pt-9 border-t-[1px] border-slate-800"
+        className="flex items-center justify-between w-full border-t-[1px] pt-9 border-slate-800"
         style={{ maxWidth: maxWidth }}
       >
         <div className="flex flex-grow items-start">
