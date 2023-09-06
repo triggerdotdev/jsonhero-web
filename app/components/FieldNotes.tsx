19e43ac2b372ee91377add206ca4d662472f0211
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2023-09-06 15:42:05 -0400

    Notes are persistent within session

--- app/components/FieldNotes.tsx
+++ app/components/FieldNotes.tsx
@@ -1,12 +1,32 @@
 import { Title } from "./Primitives/Title";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useJsonColumnViewState } from "~/hooks/useJsonColumnView";
+
+let notes: Record<string, string> = {};
 
 export function FieldNotes() {
 
-  const [value, setValue] = useState("");
+  const [note, setNote] = useState("");
+
+  function getSelectedNodeId(): string {
+    const { selectedNodeId } = useJsonColumnViewState();
+    if (!selectedNodeId) {
+      return "defaultNodeId";
+    }
+    return selectedNodeId;
+  }
+
+  const selectedNodeId: string = getSelectedNodeId();
+
+  useEffect(() => {
+    console.log("New node selected!!!!");
+    setNote(notes[selectedNodeId] || "");
+  }, [selectedNodeId]);
 
   function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setValue(event.target.value);
+    notes[selectedNodeId] = event.target.value;
+    console.log(notes);
+    setNote(event.target.value);
   }
 
   return (
@@ -16,7 +36,7 @@ export function FieldNotes() {
       </Title>
 
       <textarea
-        value={value}
+        value={note}
         onChange={handleChange}
         className="w-full h-48 px-3 py-2 text-base placeholder-gray-600 border text-white bg-slate-900 border-slate-600 focus:ring-1 focus:ring-indigo-600 focus:border-indigo-600"
         style={{resize: "none"}}
@@ -25,4 +45,3 @@ export function FieldNotes() {
     </div>
   );
 }
- 
