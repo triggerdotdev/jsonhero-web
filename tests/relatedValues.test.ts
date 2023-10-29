661d5fe50d1f9471a6f9b33ada04e01aad1af536
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-10-29 06:24:42 +0100

    Update relatedValues.test.ts with code line comments
    Added comments to  all code section

--- tests/relatedValues.test.ts
+++ tests/relatedValues.test.ts
@@ -1,16 +1,21 @@
+// Import the function for calculating related values groups
 import { calculateRelatedValuesGroups } from "../app/utilities/relatedValues";
 
+// Sample JSON data containing a list of objects
 const json = {
   data: [
+    // Object 1
     {
       stream_key: "831b5bde-cd8a-5bc4-115d-4ba34b19f481",
       status: "idle",
       reconnect_window: 60,
       playback_ids: [
+        // Playback ID 1
         {
           policy: "public",
           id: "HNRDuwff3K2VjTZZAPuvd2Kx6D01XUQFv02GFBHPUka018",
         },
+        // Playback ID 2
         {
           policy: "public",
           id: "8c836496-d923-4075-9974-3fd40dd74b03",
@@ -23,6 +28,7 @@ const json = {
       created_at: "1609937654",
       modifiedAt: "1609937654",
     },
+    // Object 2
     {
       stream_key: "d273c65e-1fc8-27dc-e9ef-56144cbceb3a",
       status: "idle",
@@ -32,10 +38,12 @@ const json = {
         "e4J9cwb5tjVxMeeV8201dC00i800ThPKKGT2SEN002dHH2s",
       ],
       playback_ids: [
+        // Playback ID 1
         {
           policy: "public",
           id: "00zOcribkUmXqXHzBTpflk2771BRTcKATqPjWf7JHpuM",
         },
+        // Playback ID 2
         {
           policy: "private",
           id: "00zOcribkUmXqXHzBTpflk2771BRTcKATqPjWf7JHpuM",
@@ -51,6 +59,7 @@ const json = {
       },
       modifiedAt: null,
     },
+    // Object 3
     {
       stream_key: "6a46c87d-00db-42a9-bad0-aad2872983bf",
       status: "playing",
@@ -60,14 +69,17 @@ const json = {
         "e4J9cwb5tjVxMeeV8201dC00i800ThPKKGT2SEN002dHH2s",
       ],
       playback_ids: [
+        // Playback ID 1
         {
           policy: "private",
           id: "00zOcribkUmXqXHzBTpflk2771BRTcKATqPjWf7JHpuM",
         },
+        // Playback ID 2
         {
           policy: "internal",
           id: "4b2aceab-f61d-499d-97d7-3f407cbcfbb6",
         },
+        // Playback ID 3
         {
           policy: { name: "shared", url: "https://example.com" },
           id: "89952227-ee16-4e4e-b15f-65c127931bcc",
@@ -85,12 +97,17 @@ const json = {
   ],
 };
 
+// Test suite for the calculateRelatedValuesGroups function
 describe("calculateRelatedValuesGroups", () => {
+  // Test case: It should return the correct values when the path is an object
   test("it should return the correct values when path is an object", () => {
+    // Define the path to a specific object property
     const path = "$.data.1.another_field";
 
+    // Calculate related values groups based on the specified path
     const result = calculateRelatedValuesGroups(path, json);
 
+    // Verify the calculated related values groups match the expected snapshot
     expect(result).toMatchInlineSnapshot(`
 Array [
   Object {
@@ -110,11 +127,15 @@ Array [
 `);
   });
 
+  // Test case: It should return the correct values when the path is an array
   test("it should return the correct values when path is an array", () => {
+    // Define the path to a specific array property
     const path = "$.data.1.recent_asset_ids";
 
+    // Calculate related values groups based on the specified path
     const result = calculateRelatedValuesGroups(path, json);
 
+    // Verify the calculated related values groups match the expected snapshot
     expect(result).toMatchInlineSnapshot(`
 Array [
   Object {
@@ -134,11 +155,15 @@ Array [
 `);
   });
 
+  // Test case: It should return the correct related values grouped by value
   test("it should return the correct related values grouped by value", () => {
+    // Define the path to a specific object property
     const path = "$.data.1.playback_ids.0.policy";
 
+    // Calculate related values groups based on the specified path
     const result = calculateRelatedValuesGroups(path, json);
 
+    // Verify the calculated related values groups match the expected values
     expect(result).toStrictEqual([
       {
         value: "public",
@@ -166,13 +191,15 @@ Array [
     ]);
   });
 
+  // Test case: It should group "undefined" separately from "null"
   test("it should group undefined separately from null", () => {
+    // Define the path to a specific object property
     const path = "$.data.1.modifiedAt";
 
+    // Calculate related values groups based on the specified path
     const result = calculateRelatedValuesGroups(path, json);
 
-    console.log(result);
-
+    // Verify the calculated related values groups match the expected values
     expect(result).toStrictEqual([
       {
         value: "1609937654",
@@ -189,3 +216,4 @@ Array [
     ]);
   });
 });
+
