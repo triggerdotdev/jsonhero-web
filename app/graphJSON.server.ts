15157053174ba7a0a79c77b2925fbde7e05a6334
Eric Allam <eallam@icloud.com>
2025-07-17 13:31:58 +0100

    Update graphJSON.server.ts
    Stop sending events go graphJSON

--- app/graphJSON.server.ts
+++ app/graphJSON.server.ts
@@ -1,27 +1,5 @@
 export async function sendEvent(event: Record<string, any>): Promise<void> {
-  if (
-    typeof GRAPH_JSON_API_KEY === "undefined" ||
-    typeof GRAPH_JSON_COLLECTION === "undefined"
-  ) {
-    return;
-  }
-
-  const payload = {
-    api_key: GRAPH_JSON_API_KEY,
-    collection: GRAPH_JSON_COLLECTION,
-    json: JSON.stringify(event),
-    timestamp: Math.floor(new Date().getTime() / 1000),
-  };
-
-  console.log(
-    `[GraphJSON] Sending event: ${JSON.stringify(payload, graphJsonReplacer)}`
-  );
-
-  await fetch("https://api.graphjson.com/api/log", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
-  });
+  return;
 }
 
 function graphJsonReplacer(key: string, value: any): any {
